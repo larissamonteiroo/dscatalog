3cd52fa3ada048e4aab413666399a379f39c74dd
Larissa Monteiro <larissa.marques.1999@hotmail.com>
2022-08-06 00:38:32 -0300

    basic beans validation

--- backend/src/main/java/com/larissa/dscatalog/dtos/UserDTO.java
+++ backend/src/main/java/com/larissa/dscatalog/dtos/UserDTO.java
@@ -4,14 +4,23 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 import com.larissa.dscatalog.entities.User;
 
 public class UserDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Long id;
+	
+	@NotBlank(message = "Campo Obrigatório")
 	private String firstName;
+	
+	@NotBlank(message = "Campo Obrigatório")	
 	private String lastName;
+	
+	@Email(message = "Insira um email válido")
 	private String email;
 	
 	Set<RoleDTO> roles = new HashSet<RoleDTO>();
